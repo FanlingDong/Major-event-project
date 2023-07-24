b0a4140d44bb003d0e17f8e7d3002711f2fd48eb
fanlingdong <z5295470@ad.unsw.edu.au>
2023-07-24 22:48:37 +1000

    get user's basic info

--- api_server/router_handler/userinfo.js
+++ api_server/router_handler/userinfo.js
@@ -1,4 +1,22 @@
+// 导入数据库操作模块
+const db = require('../db/index')
+
 // 获取用户基本信息的处理函数
 exports.getUserInfo = (req, res) => {
-    res.send('get user info ok')
+    const sql = 'SELECT id, username, nickname, email, user_pic from ev_users where id=?'
+    // 调用db.query() 执行SQL 语句
+    console.log(req.auth)
+    db.query(sql, req.auth.id, (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+        // 执行SQL语句成功，但是查询结果可能为空
+        if (results.length !== 1) return res.cc('获取用户信息失败！')
+
+        // 用户信息获取成功
+        res.send({
+            status: 0,
+            message: '获取用户基本信息成功！',
+            data: results[0],
+        })
+    })
 }
