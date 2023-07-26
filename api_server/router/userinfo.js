63357c5ba6610c5dadc91cfee7204cdb92b9b38e
fanlingdong <z5295470@ad.unsw.edu.au>
2023-07-26 23:55:01 +1000

    verify user's basic info

--- api_server/router/userinfo.js
+++ api_server/router/userinfo.js
@@ -1,6 +1,10 @@
 // 导入 express
 const express = require('express')
 const router = express()
+// 导入验证数据的中间件
+const expressJoi = require('@escook/express-joi')
+// 导入需要的验证规则对象
+const {update_userinfo_schema} = require('../schema/user')
 // 挂载路由
 
 // 导入路由处理函数模块
@@ -10,7 +14,7 @@ const userInfoHandler = require('../router_handler/userinfo')
 router.get('/userinfo', userInfoHandler.getUserInfo)
 
 // 更新用户信息的路由
-router.post('/userinfo', userInfoHandler.updateUserInfo)
+router.post('/userinfo', expressJoi(update_userinfo_schema), userInfoHandler.updateUserInfo)
 
 
 module.exports = router
--- api_server/schema/user.js
+++ api_server/schema/user.js
@@ -1,12 +1,6 @@
 // 导入定义验证规则的包
 const joi = require('joi');
 
-// 定义用户id的验证规则
-const id = joi.number()
-    .integer()
-    .min(1)
-    .required()
-
 // 定义用户名和密码的验证规则
 const username = joi.string()
     .alphanum()
@@ -17,9 +11,13 @@ const password = joi.string()
     .pattern(/^[\S]{6,12}$/)
     .required()
 
-// 定义昵称和邮箱的验证规则
+// 定义用户id，昵称nickname，邮箱email 的验证规则
+const id = joi.number()
+    .integer()
+    .min(1)
+    .required()
 const nickname = joi.string().required()
-const email = joi.string().email().required()
+const user_email = joi.string().email().required()
 
 // 定义验证注册和登录表单数据的规则对象
 exports.reg_login_schema = {
@@ -29,10 +27,12 @@ exports.reg_login_schema = {
     },
 }
 
+// 验证规则对象 - 更新用户的基本信息
 exports.update_userinfo_schema = {
+    // 需要对 req.body 里面的数据进行验证
     body: {
         id,
         nickname,
-        email,
+        email: user_email,
     },
 }
