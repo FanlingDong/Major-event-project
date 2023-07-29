6dc1832f80da7b2dde28bfee3e9a30bca8035aac
fanlingdong <z5295470@ad.unsw.edu.au>
2023-07-29 22:41:16 +1000

    update user's basic info

--- api_server/router_handler/userinfo.js
+++ api_server/router_handler/userinfo.js
@@ -23,5 +23,15 @@ exports.getUserInfo = (req, res) => {
 
 // 更新用户基本信息的处理函数
 exports.updateUserInfo = (req, res) => {
-    res.send('ok')
+    // 定义带执行的sql语句
+    const sql = 'update ev_users set ? where id=?';
+    // 调用db.query() 执行 SQL 语句并传递参数
+    db.query(sql, [req.body, req.body.id], (err, results) => {
+        // 执行 sql 语句失败了
+        if (err) return res.cc(err);
+        // 执行 SQL 语句成功，但是影响行数不等于1
+        if (results.affectedRows !== 1) return res.cc('更新用户的基本信息失败')
+        // 成功
+        res.cc('更新用户信息成功！', 0);
+    })
 }
